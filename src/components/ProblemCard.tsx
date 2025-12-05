e6306841c5a82b185f77fa2b7b6df21396e2f2e7
Cao Tung Duong <160814538+tungduong150105@users.noreply.github.com>
2025-12-05 21:54:19 +0700

    update hex of Orange color

--- src/components/ProblemCard.tsx
+++ src/components/ProblemCard.tsx
@@ -11,7 +11,7 @@ export default function ProblemCard({ value }: ProblemProp) {
     if (rating < 1200) return "#008000";
     if (rating < 1600) return "#00c0c0";
     if (rating < 2000) return "#0000ff";
-    if (rating < 2400) return "#c0c000";
+    if (rating < 2400) return "#ff8000";
     if (rating < 2800) return "#ffa500";
     return "#ff0000";
   }
