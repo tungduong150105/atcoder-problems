cddae19620532a5dcb7441fa62daf299826d624e
Cao Tung Duong <160814538+tungduong150105@users.noreply.github.com>
2025-12-05 21:52:25 +0700

    add ABC[126-129] & ABC046 & ARC062

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,6 +2,72 @@ import type { data } from "../types/problem";
 
 export const AGC_DATA: data[] = [
   {
+    title: "ABC129",
+    value: [
+      { title: "Airplane", difficult: 24, status: "Accept", link: "https://atcoder.jp/contests/abc129/tasks/abc129_a" },
+      { title: "Balance", difficult: 93, status: "Accept", link: "https://atcoder.jp/contests/abc129/tasks/abc129_b" },
+      { title: "Typical Stairs", difficult: 796, status: "Accept", link: "https://atcoder.jp/contests/abc129/tasks/abc129_c" },
+      { title: "Lamp", difficult: 1103, status: "Accept", link: "https://atcoder.jp/contests/abc129/tasks/abc129_d" },
+      { title: "Sum Equals Xor", difficult: 1547, status: "Accept", link: "https://atcoder.jp/contests/abc129/tasks/abc129_e" },
+      { title: "Takahashi's Basics in Education and Learning", difficult: 2621, status: "Accept", link: "https://atcoder.jp/contests/abc129/tasks/abc129_f" },
+    ]
+  },
+  {
+    title: "ABC128",
+    value: [
+      { title: "Apple Pie", difficult: 12, status: "Accept", link: "https://atcoder.jp/contests/abc128/tasks/abc128_a" },
+      { title: "Guidebook", difficult: 350, status: "Accept", link: "https://atcoder.jp/contests/abc128/tasks/abc128_b" },
+      { title: "Switches", difficult: 805, status: "Accept", link: "https://atcoder.jp/contests/abc128/tasks/abc128_c" },
+      { title: "equeue", difficult: 1354, status: "Accept", link: "https://atcoder.jp/contests/abc128/tasks/abc128_d" },
+      { title: "Roadwork", difficult: 1840, status: "Accept", link: "https://atcoder.jp/contests/abc128/tasks/abc128_e" },
+      { title: "Frog Jump", difficult: 2464, status: "Accept", link: "https://atcoder.jp/contests/abc128/tasks/abc128_f" },
+    ]
+  },
+  {
+    title: "ABC127",
+    value: [
+      { title: "Ferris Wheel", difficult: 15, status: "Accept", link: "https://atcoder.jp/contests/abc127/tasks/abc127_a" },
+      { title: "Algae", difficult: 26, status: "Accept", link: "https://atcoder.jp/contests/abc127/tasks/abc127_b" },
+      { title: "Prison", difficult: 239, status: "Accept", link: "https://atcoder.jp/contests/abc127/tasks/abc127_c" },
+      { title: "Integer Cards", difficult: 887, status: "Accept", link: "https://atcoder.jp/contests/abc127/tasks/abc127_d" },
+      { title: "Cell Distance", difficult: 1938, status: "Accept", link: "https://atcoder.jp/contests/abc127/tasks/abc127_e" },
+      { title: "Absolute Minima", difficult: 2003, status: "Accept", link: "https://atcoder.jp/contests/abc127/tasks/abc127_f" },
+    ]
+  },
+  {
+    title: "ABC126",
+    value: [
+      { title: "Changing a Character", difficult: 0, status: "Accept", link: "https://atcoder.jp/contests/abc126/tasks/abc126_a" },
+      { title: "YYMM or MMYY", difficult: 0, status: "Accept", link: "https://atcoder.jp/contests/abc126/tasks/abc126_b" },
+      { title: "Dice and Coin", difficult: 638, status: "Accept", link: "https://atcoder.jp/contests/abc126/tasks/abc126_c" },
+      { title: "Even Relation", difficult: 1279, status: "Accept", link: "https://atcoder.jp/contests/abc126/tasks/abc126_d" },
+      { title: "1 or 2", difficult: 1340, status: "Accept", link: "https://atcoder.jp/contests/abc126/tasks/abc126_e" },
+      { title: "XOR Matching", difficult: 1770, status: "Accept", link: "https://atcoder.jp/contests/abc126/tasks/abc126_f" },
+    ]
+  },
+  {
+    title: "ARC104",
+    value: [
+      { title: "Plus Minus", difficult: 13, status: "Accept", link: "https://atcoder.jp/contests/arc104/tasks/arc104_a" },
+      { title: "DNA Sequence", difficult: 487, status: "Accept", link: "https://atcoder.jp/contests/arc104/tasks/arc104_b" },
+      { title: "Fair Elevator", difficult: 2009, status: "Accept", link: "https://atcoder.jp/contests/arc104/tasks/arc104_c" },
+      { title: "Multiset Mean", difficult: 2251, status: "Accept", link: "https://atcoder.jp/contests/arc104/tasks/arc104_d" },
+      { title: "Random LIS", difficult: 2824, status: "Accept", link: "https://atcoder.jp/contests/arc104/tasks/arc104_e" },
+      { title: "Visibility Sequence", difficult: 3213, status: "", link: "https://atcoder.jp/contests/arc104/tasks/arc104_f" },
+    ]
+  },
+  {
+    title: "ABC046 & ARC062",
+    value: [
+      { title: "AtCoDeer and Paint Cans", difficult: 26, link: "https://atcoder.jp/contests/abc046/tasks/abc046_a" },
+      { title: "Painting Balls with AtCoDeer", difficult: 633, link: "https://atcoder.jp/contests/abc046/tasks/abc046_b" },
+      { title: "AtCoDeer and Election Report", difficult: 1346, status: "", link: "https://atcoder.jp/contests/arc062/tasks/arc062_a" },
+      { title: "AtCoDeer and Rock-Paper", difficult: 1256, status: "", link: "https://atcoder.jp/contests/arc062/tasks/arc062_b" },
+      { title: "Building Cubes with AtCoDeer", difficult: 2655, status: "Accept", link: "https://atcoder.jp/contests/arc062/tasks/arc062_c" },
+      { title: "Painting Graphs with AtCoDeer", difficult: 3175, status: "", link: "https://atcoder.jp/contests/arc062/tasks/arc062_d" },
+    ]
+  },
+  {
     title: "ABC045 & ARC061",
     value: [
       { title: "Trapezoids", difficult: 26, link: "https://atcoder.jp/contests/abc045/tasks/abc045_a" },
